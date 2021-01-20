e733458439515f82ddbd9e9ddf5b626572d9374c
Ismaylwo <77680125+Ismaylwo@users.noreply.github.com>
2021-01-20 13:51:19 +0500

    Add files via upload

---
+++ DZ5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DZ5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = 10985;
+            Console.WriteLine("Результат :" + N / 60);
+        }
+    }
+}

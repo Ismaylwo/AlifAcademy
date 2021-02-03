27223234393c0aa490b2693c963792b37357a914
Ismoil <77680125+Ismaylwo@users.noreply.github.com>
2021-02-03 21:59:42 +0500

    Add files via upload

---
+++ 02-02-21/dz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace dz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class worker{
+        public int id{get; set;}
+        public string name{get; set;}
+        public string LastName{get; set;}
+        public string middleName{get; set;}
+        public int age {get; set;}
+        public string BirthOfDay{get; set;}
+        public string Adress{get; set;}
+        public string Position{get; set;}
+        
+    }
+}
